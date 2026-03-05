9e147598caf881f532e2ec065f439906f453237e
Peter Hueer <peter.hueer@adesso.de>
2026-03-05 18:01:03 +0100

    feat: change port from 3000 to 7777
    Import PORT from lib/constants instead of hardcoding it in server.js.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
+const { PORT } = require('./lib/constants');
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
